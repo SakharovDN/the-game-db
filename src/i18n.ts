954e4c39506f3e445a7fe5332a91f8593ed9efda
SakharovDN <deafsah@gmail.com>
2024-08-03 21:09:07 +0700

    Revert "fix(core): fixed asset paths"
    This reverts commit 2617dcef3d1a783461c30383a14a82aabe59b7be.

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,6 +4,8 @@ import i18n from 'i18next';
 import HttpBackend from 'i18next-http-backend';
 import ICU from 'i18next-icu';
 
+import { environment } from './environment';
+
 export enum Language {
   En = 'en',
   Ru = 'ru',
@@ -15,7 +17,7 @@ i18n
   .use(HttpBackend)
   .init({
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: `${environment.baseUrl}locales/{{lng}}/{{ns}}.json`,
     },
     debug: true,
     defaultNS: 'languages',
